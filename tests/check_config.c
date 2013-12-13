64f569a91c73e748b0b2710262d86cf32594978d
Giacomo Bagnoli <gbagnoli@gmail.com>
2013-12-13 14:28:09 +0000

    Added first easy tests for config

--- tests/check_config.c
+++ tests/check_config.c
@@ -3,11 +3,60 @@
 
 /* include the header(s) of the tested code right after */
 #include <config_private.h>
+#include <logger.h>
+
+#define S "sect"
+#define N "name"
+#define PS PARAM_STRING
+
+#define ck_assert_call_ok(fun, ...) ck_assert_int_eq(fun(__VA_ARGS__), 0);
+#define ck_assert_call_fail(fun, ...) ck_assert_int_eq(fun(__VA_ARGS__), 1);
 
 
-START_TEST(test_config_api)
+struct Config *conf;
+
+void
+setup(void)
 {
-  return;/* fill me with a meaningful test! */
+   struct Logger *logger = logger_open_console(LOG_LAST, stderr);
+   conf = config_new(logger);
+}
+
+void
+teardown(void)
+{
+    config_destroy(conf);
+}
+
+
+START_TEST(test_config_create)
+{
+    char *value;
+    long v;
+    ck_assert(conf != NULL);
+    // missing required arguments
+    ck_assert_call_fail(config_opt_add, NULL, S, N, PS, NULL);
+    ck_assert_call_fail(config_opt_add, conf, NULL, N, PS, NULL);
+    ck_assert_call_fail(config_opt_add, conf, S, NULL, PS, NULL);
+
+    // setting properties on non-existing option
+    ck_assert_call_fail(config_opt_set_range_int, conf, S, N, 0, 1);
+    ck_assert_call_fail(config_opt_set_multivalued, conf, S, N, 0);
+    ck_assert_call_fail(config_opt_set_default_int, conf, S, N, 1);
+    ck_assert_call_fail(config_opt_set_default_string, conf, S, N, "a");
+    ck_assert_call_fail(config_opt_set_default_string, conf, S, N, "a");
+    // retrieving values from a non-existing option
+    ck_assert_call_fail(config_get_nth_bool, conf, S, N, 0, (int *)&v);
+    ck_assert_call_fail(config_get_nth_int, conf, S, N, 0, &v);
+    ck_assert_call_fail(config_get_nth_string, conf, S, N, 0, &value);
+    ck_assert(value == NULL);
+    return;
+}
+END_TEST
+
+START_TEST(test_config_opt_add)
+{
+    ck_assert_call_ok(config_opt_add, conf, S, N, PARAM_STRING, "tests");
 }
 END_TEST
 
@@ -18,7 +67,9 @@ config_suite(void)
   Suite *s = suite_create("rapp.core.config");
   TCase *tc = tcase_create("rapp.core.config");
 
-  tcase_add_test(tc, test_config_api);
+  tcase_add_checked_fixture (tc, setup, teardown);
+  tcase_add_test(tc, test_config_create);
+  tcase_add_test(tc, test_config_opt_add);
   suite_add_tcase(s, tc);
 
   return s;
@@ -31,7 +82,7 @@ main (void)
 
  Suite *s = config_suite();
  SRunner *sr = srunner_create(s);
-
+ srunner_set_log(sr, "check_config.log");
  srunner_run_all(sr, CK_NORMAL);
  number_failed = srunner_ntests_failed(sr);
  srunner_free(sr);
