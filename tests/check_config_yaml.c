0ef8bb43cd841897525ed8545a070f87ac8b1f7a
Giacomo Bagnoli <gbagnoli@gmail.com>
2013-12-14 13:07:51 +0000

    More yaml tests

--- tests/check_config_yaml.c
+++ tests/check_config_yaml.c
@@ -7,6 +7,7 @@
 #include <logger.h>
 
 #include "test_utils.h"
+#include "yaml_documents.h"
 
 #define CHECK_STR(CONF, SECT, NAME, STR, EXPECTED, POS)         \
 do {                                                            \
@@ -22,24 +23,6 @@ do {                                                            \
 } while(0)
 
 struct Config *conf;
-const char *yaml_good =
-"---\n"
-"core:\n"
-"    address : \"127.0.0.1\"\n"
-"    port: 8080 \n"
-"    test_list:\n"
-"        - first\n"
-"        - second\n"
-"        - third\n"
-"    test_bool: y\n"
-"    test_list_inline: [1,2,3]\n"
-"skipped:\n"
-"    this: \"is skipped\"\n"
-"    with_all: \"options\"\n";
-
-const char *yaml_good_inline =
-"---\n"
-"core: {address: \"127.0.0.2\", port: 8081, test_list: [\"first1\", \"second1\", \"third1\"], test_bool: no, test_list_inline: [4,5,6]}";
 
 void
 setup(void)
@@ -59,33 +42,66 @@ START_TEST(test_config_yaml_parse)
     long vi;
     char *vs;
     const char *c = "core";
+    struct Logger *logger = logger_open_console(LOG_LAST, stderr);
+    struct Config *conf2 = config_new(logger);
+    struct Config *cp;
+    const char *yaml;
+    int i;
 
-    config_opt_add(conf, c, "address", PARAM_STRING, NULL);
-    config_opt_add(conf, c, "port", PARAM_INT, NULL);
-    config_opt_set_range_int(conf, c, "port", 1, 65535);
-    config_opt_add(conf, c, "test_list", PARAM_STRING, NULL);
-    config_opt_add(conf, c, "test_list_inline", PARAM_INT, NULL);
-    config_opt_add(conf, c, "test_bool", PARAM_BOOL, NULL);
-    config_opt_set_multivalued(conf, c, "test_list", 1);
-    config_opt_set_multivalued(conf, c, "test_list_inline", 1);
 
-    config_parse_string(conf, yaml_good);
-    CHECK_STR(conf, c, "address", vs, "127.0.0.1", 0);
-    CHECK_INT(conf, c, "port", vi, 8080, 0);
-    CHECK_STR(conf, c, "test_list", vs, "first", 0);
-    CHECK_STR(conf, c, "test_list", vs, "second", 1);
-    CHECK_STR(conf, c, "test_list", vs, "third", 2);
-    CHECK_INT(conf, c, "test_bool", vi, 1, 0);
-    CHECK_INT(conf, c, "test_list_inline", vi, 1, 0);
-    CHECK_INT(conf, c, "test_list_inline", vi, 2, 1);
-    CHECK_INT(conf, c, "test_list_inline", vi, 3, 2);
-
-    config_parse_string(conf, yaml_good_inline);
-
+    for (i=0; i<2; i++) {
+        if (i == 0) {
+            cp = conf;
+            yaml = yaml_good;
+        } else {
+            cp = conf2;
+            yaml = yaml_good_inline;
+        }
+        config_opt_add(cp, c, "address", PARAM_STRING, NULL);
+        config_opt_add(cp, c, "port", PARAM_INT, NULL);
+        config_opt_set_range_int(cp, c, "port", 1, 65535);
+        config_opt_add(cp, c, "test_list", PARAM_STRING, NULL);
+        config_opt_add(cp, c, "test_list_inline", PARAM_INT, NULL);
+        config_opt_add(cp, c, "test_bool", PARAM_BOOL, NULL);
+        config_opt_set_multivalued(cp, c, "test_list", 1);
+        config_opt_set_multivalued(cp, c, "test_list_inline", 1);
+        ck_assert_call_ok(config_parse_string, cp, yaml);
+        CHECK_STR(cp, c, "address", vs, "127.0.0.1", 0);
+        CHECK_INT(cp, c, "port", vi, 8080, 0);
+        CHECK_STR(cp, c, "test_list", vs, "first", 0);
+        CHECK_STR(cp, c, "test_list", vs, "second", 1);
+        CHECK_STR(cp, c, "test_list", vs, "third", 2);
+        CHECK_INT(cp, c, "test_bool", vi, 1, 0);
+        CHECK_INT(cp, c, "test_list_inline", vi, 1, 0);
+        CHECK_INT(cp, c, "test_list_inline", vi, 2, 1);
+        CHECK_INT(cp, c, "test_list_inline", vi, 3, 2);
+    }
     return;
 }
 END_TEST
 
+START_TEST(test_yaml_empty)
+{
+    config_opt_add(conf, "core", "address", PARAM_STRING, NULL);
+    ck_assert_call_fail(config_parse_string, conf, yaml_no_stream);
+    ck_assert_call_fail(config_parse_string, conf, yaml_empty_document);
+    ck_assert_call_fail(config_parse_string, conf, yaml_document_no_mapping);
+    ck_assert_call_fail(config_parse_string, conf, yaml_no_start_document);
+}
+END_TEST
+
+START_TEST(test_yaml_invalid_data)
+{
+    config_opt_add(conf, "core", "intvalue", PARAM_INT, NULL);
+    config_opt_add(conf, "core", "boolvalue", PARAM_BOOL, NULL);
+    config_opt_add(conf, "core", "strvalue", PARAM_STRING, NULL);
+    ck_assert_call_fail(config_parse_string, conf, yaml_wrong_int);
+    ck_assert_call_fail(config_parse_string, conf, yaml_wrong_int2);
+    ck_assert_call_fail(config_parse_string, conf, yaml_wrong_bool);
+    ck_assert_call_fail(config_parse_string, conf, yaml_wrong_string);
+
+}
+END_TEST
 
 static Suite *
 config_yaml_parse_suite(void)
@@ -95,6 +111,8 @@ config_yaml_parse_suite(void)
 
   tcase_add_checked_fixture (tc, setup, teardown);
   tcase_add_test(tc, test_config_yaml_parse);
+  tcase_add_test(tc, test_yaml_empty);
+  tcase_add_test(tc, test_yaml_invalid_data);
   suite_add_tcase(s, tc);
 
   return s;
---
+++ tests/yaml_documents.h
@@ -0,0 +1,27 @@
+const char *yaml_good =
+"---\n"
+"core:\n"
+"    address : \"127.0.0.1\"\n"
+"    port: 8080\n"
+"    test_list:\n"
+"        - first\n"
+"        - second\n"
+"        - third\n"
+"    test_bool: yes\n"
+"    test_list_inline: [1,2,3]\n"
+"skipped:\n"
+"    this: \"is skipped\"\n"
+"    with_all: \"options\"\n";
+
+const char *yaml_good_inline =
+"---\n"
+"core: {address: \"127.0.0.1\", port: 8080, test_list: [\"first\", \"second\", \"third\"], test_bool: on, test_list_inline: [1,2,3]}";
+
+const char *yaml_no_stream = "";
+const char *yaml_empty_document = "---\n";
+const char *yaml_document_no_mapping = "---\n[1,2,3]";
+const char *yaml_no_start_document = "core: {address: \"0.0.0.0\"}";
+const char *yaml_wrong_int = "---\ncore: {intvalue: absderrg}";
+const char *yaml_wrong_int2 = "---\ncore: {intvalue: 100abc}";
+const char *yaml_wrong_bool = "---\ncore: {boolvalue: absderrg}";
+const char *yaml_wrong_string = "---\ncore: {strvalue: }";
