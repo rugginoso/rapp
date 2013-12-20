99508ab1da9ce318884867c12fd300f6e2b9f72b
Francesco Romani <fromani@gmail.com>
2013-12-20 21:32:12 +0100

    extend container API for httprouter needs.
    be more strict during container creation for allocation failures.
    add function for create a static, custom container: will be needed
    by httprouter tests.
    reimplement null container in terms of new custom container API.

--- src/container.h
+++ src/container.h
@@ -6,6 +6,10 @@
 #include "rapp/rapp.h"
 
 
+typedef int (*RappServeCallback)(struct RappContainer *handle, struct HTTPRequest *http_request, struct HTTPResponseWriter *response_writer);
+
+typedef int (*RappDestroyCallback)(struct RappContainer *handle);
+
 struct Container;
 
 struct Container *container_new(struct Logger *logger, const char *name, int ac, char **av);
@@ -13,6 +17,7 @@ void container_destroy(struct Container *container);
 
 int container_serve(struct Container *container, struct HTTPRequest *http_request, struct HTTPResponseWriter *response_writer);
 
-struct Container *container_null(struct Logger *logger);
+struct Container *container_new_null(struct Logger *logger, const char *tag);
+struct Container *container_new_custom(struct Logger *logger, const char *tag, RappServeCallback serve, RappDestroyCallback destroy, void *user_data);
 
 #endif /* CONTAINER_H */
