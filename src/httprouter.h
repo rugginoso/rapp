6fe518bd5b2513e8807626c129b307812ca3a77a
Francesco Romani <fromani@gmail.com>
2013-12-21 09:32:13 +0100

    rename the setter of the default container

--- src/httprouter.h
+++ src/httprouter.h
@@ -11,7 +11,7 @@ struct HTTPRouter;
 struct HTTPRouter *http_router_new(struct Logger *logger);
 void http_router_destroy(struct HTTPRouter *router);
 
-int http_router_default_container(struct HTTPRouter *router, struct Container *container);
+int http_router_set_default_container(struct HTTPRouter *router, struct Container *container);
 
 int http_router_bind(struct HTTPRouter *router, const char *route, struct Container *container);
 
