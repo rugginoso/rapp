060710880253fd3438fc9b81db04e9ab4228d9bf
Francesco Romani <fromani@gmail.com>
2013-12-21 12:07:28 +0100

    add the source code decorator script
    used in commit ab1f3cdf719fe6e8f7a8bce51edb37fb840cef3d
    to decorate the source files.

---
+++ contrib/cdeco.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+#
+# put in the public domain
+
+import os
+import os.path
+import sys
+
+
+def decorate(srcpath):
+    srcname = os.path.basename(srcpath)
+    header = [
+        "/*\n",
+        " * %s - is part of RApp.\n" % srcname,
+        " * RApp is a modular web application container made for linux and for speed.\n",
+        " * (C) 2013 the RApp devs. Licensed under GPLv2 with additional rights.\n",
+        " *     see LICENSE for all the details.\n",
+        " */\n"
+    ]
+    trailer = [
+        "/*\n",
+        " * vim: expandtab shiftwidth=2 tabstop=2:\n",
+        " */\n"
+    ]
+    with open(srcpath, "rt") as src:
+        content = src.readlines()
+    blankline = ["\n"]
+    separator = [] if content[0] == "\n" else blankline
+    with open(srcpath, "wt") as dst:
+        dst.writelines(header
+                     + separator
+                     + content
+                     + trailer
+                     + blankline)
+
+
+def _main():
+    for src in sys.stdin:
+        decorate(src.strip())
+
+
+if __name__ == "__main__":
+    _main()
