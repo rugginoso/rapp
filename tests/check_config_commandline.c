1b60d02547f35b1b5cd22fd53c9091ae9177f2b0
Giacomo Bagnoli <gbagnoli@gmail.com>
2013-12-23 17:48:12 +0000

    Added tests for early commandline options

--- tests/check_config_commandline.c
+++ tests/check_config_commandline.c
@@ -5,6 +5,7 @@
  *     see LICENSE for all the details.
  */
 
+#include <errno.h>
 #include <stdlib.h>
 #include <check.h>
 
@@ -19,6 +20,38 @@
 
 
 struct Config *conf;
+char *empty[] = {"rapp", NULL};
+char *usage[] = {"rapp", "--usage", NULL};
+char *version[] = {"rapp", "--version", NULL};
+char *help[] = {"rapp", "--help", NULL};
+
+char**
+copy_argv(int argc, char *argv[])
+{
+  char **res;
+  int i;
+  size_t len;
+  res = calloc(argc, sizeof(char*));
+  for (i=0; i<argc; i++) {
+    if (argv[i]) {
+      len = strlen(argv[i]) + 1;
+      res[i] = malloc(len);
+      strncpy(res[i], argv[i], len);
+    }
+  }
+  return res;
+}
+
+void
+clear_argv(int argc, char *argv[])
+{
+  int i;
+  for (i=0; i<argc; i++) {
+    if (argv[i])
+      free(argv[i]);
+  }
+  free(argv);
+}
 
 void
 setup(void)
@@ -33,9 +66,59 @@ teardown(void)
   config_destroy(conf);
 }
 
+void
+test_loglevel(struct RappArguments *arguments, int pos,
+              int argc, char **argv, const char *value, int expected)
+{
+  argv[pos] = strdup(value);
+  ck_assert_call_ok(config_parse_early_commandline, arguments, 3, argv);
+  ck_assert_int_eq(arguments->loglevel, expected);
+  free(argv[pos]);
+}
+
 START_TEST(test_config_early_options)
 {
-  return;
+  struct RappArguments arguments;
+  char **args;
+  char *loglevel[] = {"rapp", "--log-level", NULL, NULL};
+  char *logout[] = {"rapp", "--log-output", "-", NULL};
+  char *load[] = {"rapp", "--load", NULL, NULL};
+  char *logcolor[] = {"rapp", "--log-nocolor"};
+  ck_assert_call_fail(config_parse_early_commandline, &arguments, 0, 0);
+  ck_assert_call_fail(config_parse_early_commandline, &arguments, 1, 0);
+  ck_assert_call_ok(config_parse_early_commandline, &arguments, 1, empty);
+  // defaults:
+  ck_assert_int_eq(arguments.loglevel, LOG_INFO);
+  ck_assert_int_eq(arguments.lognocolor, 0);
+  ck_assert(arguments.logoutput == NULL);
+  ck_assert(arguments.container == NULL);
+
+  // argp arguments should not be defined (EINVAL)
+  ck_assert_int_eq(config_parse_early_commandline(&arguments, 2, usage), EINVAL);
+  ck_assert_int_eq(config_parse_early_commandline(&arguments, 2, version), EINVAL);
+  ck_assert_int_eq(config_parse_early_commandline(&arguments, 2, help), EINVAL);
+
+  // loglevel null arg
+  ck_assert_call_fail(config_parse_early_commandline, &arguments, 3, loglevel);
+  test_loglevel(&arguments, 2, 3, loglevel, "DEBUG", LOG_DEBUG);
+  test_loglevel(&arguments, 2, 3, loglevel, "debug", LOG_DEBUG);
+  test_loglevel(&arguments, 2, 3, loglevel, "info", LOG_INFO);
+  test_loglevel(&arguments, 2, 3, loglevel, "warn", LOG_WARNING);
+  test_loglevel(&arguments, 2, 3, loglevel, "error", LOG_ERROR);
+  test_loglevel(&arguments, 2, 3, loglevel, "critical", LOG_CRITICAL);
+  // unkown values defaults to LOG_INFO
+  test_loglevel(&arguments, 2, 3, loglevel, "other", LOG_INFO);
+
+  // TODO
+  ck_assert_call_ok(config_parse_early_commandline, &arguments, 3, logout);
+
+  ck_assert_call_ok(config_parse_early_commandline, &arguments, 2, logcolor);
+  ck_assert_int_eq(arguments.lognocolor, 1);
+
+  ck_assert_call_fail(config_parse_early_commandline, &arguments, 3, load);
+  load[2] = strdup("path/to/container.so");
+  ck_assert_call_ok(config_parse_early_commandline, &arguments, 3, load);
+  ck_assert_str_eq(arguments.container, "path/to/container.so");
 }
 END_TEST
 
