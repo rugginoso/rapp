c2ade4dc7cbfe64c7b790702184a95b10a028df2
Francesco Romani <fromani@gmail.com>
2013-12-25 13:42:05 +0100

    make tcpserver test independent from the local DNS
    previously, check_tcpserver could have failed for
    a testing host DNS (mis)configuration.
    
    Now, getaddrinfo() is stubbed, so the tests are independent
    and encapsulated.

--- tests/check_tcpserver.c
+++ tests/check_tcpserver.c
@@ -5,7 +5,12 @@
  *     see LICENSE for all the details.
  */
 
+#include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+
 #include <check.h>
 
 #include <logger.h>
@@ -18,10 +23,77 @@
 #define PORT 8000
 
 
-struct Logger *logger = NULL;
-struct ELoop *eloop = NULL;
-struct TcpServer *tcp_server = NULL;
-struct TcpConnection *tcp_connection = NULL;
+static struct Logger *logger = NULL;
+static struct ELoop *eloop = NULL;
+static struct TcpServer *tcp_server = NULL;
+static struct TcpConnection *tcp_connection = NULL;
+static int gai_errno = 0;
+
+static struct addrinfo *
+allocaddrinfo(void)
+{
+  struct addrinfo *out = calloc(1, sizeof(struct addrinfo));
+  assert(out);
+  out->ai_addr = calloc(1, sizeof(struct sockaddr_in));
+  assert(out->ai_addr);
+  return out;
+}
+
+static void
+filladdr4(struct addrinfo *out,
+          const char      *host,
+          const char      *port)
+{
+  struct sockaddr_in *addr4 = (struct sockaddr_in *)out->ai_addr;
+  addr4->sin_family = AF_INET;
+  inet_pton(out->ai_family, host, &(addr4->sin_addr));
+  addr4->sin_port = htons(atoi(port));
+
+  out->ai_addrlen = sizeof(struct sockaddr_in);
+  out->ai_family = addr4->sin_family;
+}
+
+int getaddrinfo(const char *node, const char *service,
+                const struct addrinfo *hints,
+                struct addrinfo **res)
+{
+  /* not assert() because we want to deal with error cases */
+  int ret = -1;
+  if (node != NULL && service != NULL && res != NULL) {
+    if (strcmp(node, "localhost") == 0) {
+      struct addrinfo *out = allocaddrinfo();
+      out->ai_socktype = SOCK_STREAM;
+      filladdr4(out, "127.0.0.1", service);
+      *res = out;
+      ret = 0;
+    } else if (strcmp(node, "notexistent") == 0) {
+      gai_errno = EAI_FAIL;
+    }
+  }
+  return ret;
+}
+
+void freeaddrinfo(struct addrinfo *res)
+{
+  if (res) {
+    free(res->ai_addr);
+    free(res);
+  }
+}
+
+const char *gai_strerror(int errcode)
+{
+  const char *desc = NULL;
+  switch (errcode) {
+    case EAI_FAIL:
+      desc = "The name server returned a permanent failure indication.";
+      break;
+    default:
+      desc = "Unknown error.";
+      break;
+  }
+  return desc;
+}
 
 
 static void
@@ -55,12 +127,15 @@ void teardown(void)
 
 START_TEST(test_tcp_server_calls_callback_when_accepts_new_connections)
 {
+  int ret;
   int client_fd = -1;
 
   tcp_server_set_accept_callback(tcp_server, accept_func, eloop);
-  tcp_server_start_listen(tcp_server, HOST, PORT);
+  ret = tcp_server_start_listen(tcp_server, HOST, PORT);
+  ck_assert(ret == 0);
 
-  ck_assert(connect_to(HOST, PORT) >= 0);
+  ret = connect_to(HOST, PORT);
+  ck_assert(ret >= 0);
 
   event_loop_run(eloop);
 
@@ -70,15 +145,18 @@ END_TEST
 
 START_TEST(test_tcp_server_dont_bind_on_not_existent_address)
 {
-  ck_assert(tcp_server_start_listen(tcp_server, "notexistent", PORT) != 0);
+  int ret = tcp_server_start_listen(tcp_server, "notexistent", PORT);
+
+  ck_assert(ret != 0);
 }
 END_TEST
 
 START_TEST(test_tcp_server_dont_bind_on_used_port)
 {
   int other_server_fd = listen_to(HOST, PORT);
+  int ret = tcp_server_start_listen(tcp_server, HOST, PORT);
 
-  ck_assert(tcp_server_start_listen(tcp_server, HOST, PORT) != 0);
+  ck_assert(ret != 0);
 
   close(other_server_fd);
 }
@@ -94,6 +172,7 @@ tcp_server_suite(void)
   tcase_add_test(tc, test_tcp_server_calls_callback_when_accepts_new_connections);
   tcase_add_test(tc, test_tcp_server_dont_bind_on_not_existent_address);
   tcase_add_test(tc, test_tcp_server_dont_bind_on_used_port);
+
   suite_add_tcase(s, tc);
 
   return s;
