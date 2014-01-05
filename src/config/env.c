f41ab533e81f79b8958df3c53c17eea6a0346ec6
Francesco Romani <fromani@gmail.com>
2014-01-05 13:17:19 +0100

    simplify code leveraging memory_asprintf

--- src/config/env.c
+++ src/config/env.c
@@ -11,30 +11,18 @@
 #include "src/memory.h"
 #include "common.h"
 
-#define PREFIX "RAPP_"
-#define PREFIX_LEN 5
 
 static int
 get_env_name(const char *sect,
              const char *name,
              char **envname)
 {
-  size_t len = 0;
-  if (strcmp(sect, RAPP_CONFIG_SECTION) != 0) {
-    len = (strlen(name) + strlen(sect) + PREFIX_LEN + 2);
-    *envname = memory_create(len);
-    if (!*envname)
-      return -1;
-    snprintf(*envname, len, "%s%s_%s", PREFIX, sect, name);
-
-  } else {
-    len = (strlen(name) + PREFIX_LEN + 1);
-    *envname = malloc(len);
-    if (!*envname)
-      return -1;
-    snprintf(*envname, len, "%s%s", PREFIX, name);
-  }
-
+  int is_core = (strcmp(sect, RAPP_CONFIG_SECTION) == 0);
+  if (memory_asprintf(envname, "RAPP_%s%s%s",
+                      (is_core) ?"" :sect,
+                      (is_core) ?"" :"_",
+                      name) < 0)
+    return -1;
   uppercase(*envname);
   return 0;
 }
@@ -92,8 +80,7 @@ get_env_for_opt(struct RappConfig   *conf,
     res = config_add_value_from_string(conf, opt, value);
   }
 
-  // set no override so values in config files do not
-  // override env value;
+  /* set no override so values in config files do not override env value */
   if (res == 0)
     opt->no_override = 1;
 
@@ -122,3 +109,4 @@ config_read_env(struct RappConfig *conf)
 /*
  * vim: expandtab shiftwidth=2 tabstop=2:
  */
+
